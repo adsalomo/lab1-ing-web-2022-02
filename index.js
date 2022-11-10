e6da35bd0bd0dda3049372f482683210967c1080
adsalomo <adsalomo@gmail.com>
2022-11-09 20:32:29 -0500

    inicializacion del proyecto

---
+++ index.js
@@ -0,0 +1,31 @@
+//const componenteHTML = document.getElementsByClassName('card');
+//console.log(componenteHTML); // System.out.println("");
+
+// tipos de datos java script scope
+
+var edad; // undefined
+console.log(edad);
+
+var genero = 50; // 
+console.log(genero);
+
+// const let
+
+let nombre = 'Adrian';
+console.log(nombre);
+
+if (1 === 1) {
+    let nombre = 'John'
+}
+
+console.log(nombre);
+
+const persona = 'Juan';
+//const persona;
+//persona = 'Pedro';
+
+let apellido;
+apellido = 'Vallejo';
+apellido = 50;
+
+console.log(apellido);
